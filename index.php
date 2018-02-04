78721c3c0659aee90178de9af302c087e4ff298d
Drew Budwin <drew@budw.in>
2018-02-04 15:14:41 -0500

    Update layout, upgrade to Bootstrap 4
    Updated to Bootstrap 4 and made changes to the layout to make it work with Bootstrap 4.  Also switched to using a CDN instead of hosting 3rd party files myself.

--- index.php
+++ index.php
@@ -2,29 +2,23 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Welcome to Budw.in</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link href="css/style.css" rel="stylesheet">
-        <link href="css/ionicons.min.css" rel="stylesheet">
+        <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
+        <title>Welcome to Budw.in</title>
     </head>
     <body>
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-xs-12 col-md-12">
-                    <nav class="navbar navbar-default" role="navigation">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-                            </button> <a class="navbar-brand" href="http://www.budw.in">Budw.in</a>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-            <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="https://www.budw.in">Budw.in</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+            <div class="row mt-4">
                 <div class="col-xs-12 col-md-5">
-                    <img src="images/DrewCartoonNoBackground.png" class="img-responsive img-circle" alt="That's me!">
+                    <img src="images/DrewCartoonNoBackground.png" class="img-fluid rounded" alt="That's me!">
                 </div>
                 <div class="col-xs-12 col-md-7">
                     <div class="jumbotron">
@@ -34,68 +28,97 @@
                         <p>
                             You've somehow stumbled onto this corner of the internet.  Not a whole lot goes on here, it's mainly just used for managing my family's email along with being a playground for web development software, like this <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a> page.  But since you're here, I figured I'd put up some content to help spruce things up a little bit.  By trade I'm a software engineer, but I also have a passion for woodworking as a side job.  I'm also known to be a collector of hobbies which has led me to have wide and varied interests.
                         </p>
-                        <a href="#" target="_blank" class="btn btn-primary btn-large" role="button">
-                            <span class="glyphicon glyphicon-education" aria-hidden="true"></span>
-                                My Résumé
+                        <p>
+                            If you would like to contact me about job opportunities or partnerships, please do so by contacting me on <a href="https://www.linkedin.com/in/dbudwin" target="_blank">LinkedIn</a>.
+                        </p>
+                        <a href="https://resume.github.io/?dbudwin" target="_blank" class="btn btn-info btn-large" role="button">
+                            <span class="ion-social-github" aria-hidden="true"></span> My GitHub Résumé
+                        </a>
+                        <a href="https://www.linkedin.com/in/dbudwin" target="_blank" class="btn btn-info btn-large" role="button">
+                            <span class="ion-social-linkedin" aria-hidden="true"></span> My LinkedIn
                         </a>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-xs-12 col-md-7">
-                    <h2>
+            <div class="row mt-4">
+                <div class="col-xs-12 col-md-7 order-md-first">
+                    <h2 class="text-white">
                         Software
                     </h2>
-                    <p>
-                        I'm a software engineer for a living and graduated from <a href="https://www.vt.edu/" target="_blank">Virginia Tech</a> with a degree in computer science and a minor in math.  I've started some open-source projects that I've shared on GitHub.  I got started in software when I really wanted to learn how to make a website to track all my comic books when I was 12 years old.  My first websites were  built using a mixture of HTML, CSS, PHP, and MySQL--through this, a love of software engineering was born.  When it comes to software I'm most proficient with C++ (<a href="http://www.qt.io/" target="_blank">Qt</a>), C#, and Java.  I also really enjoy working with <a href="https://www.arduino.cc/" target="_blank">Arduinos</a> and other microcontrollers like the <a href="http://www.nodemcu.com/index_en.html" target="_blank">ESP8266/NodeMCU</a>.  Lastly, I'm a big fan of good processes and <a href="https://cleancoders.com/" target="_blank">Clean Code</a>.
-                    </p>
-                    <div class="text-center">
-                        <a href="https://github.com/dbudwin" target="_blank" class="btn btn-primary" role="button">
-                            <span class="glyphicon glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>
+                    <div class="p-3 mb-2 bg-dark text-white">
+                        <p>
+                            I'm a software engineer for a living and graduated from <a href="https://www.vt.edu/" class="text-info" target="_blank">Virginia Tech's</a> College of Engineering with a degree in computer science and a minor in math.  I've started some open-source projects that I've shared on GitHub and contributed to many others.  I got started in software when I really wanted to learn how to make a website to track all my comic books when I was 12 years old.  My first websites were built using a mixture of HTML, CSS, PHP, and MySQL--through this, a love of software engineering was born.
+                        </p>
+                        <p>
+                            Some technologies I regularly work with:
+                            <ul>
+                                <li>
+                                    PHP - <a href="https://laravel.com" class="text-info" target="_blank">Laravel</a>, <a href="https://phpunit.de" class="text-info"  target="_blank">PHPUnit</a>, <a href="https://github.com/mockery/mockery" class="text-info" target="_blank">Mockery</a>
+                                </li>
+                                <li>
+                                    C++ - <a href="https://www.qt.io" class="text-info" target="_blank">Qt</a>, <a href="https://github.com/google/googletest" class="text-info" target="_blank">Google Test/Google Mock</a>, <a href="https://github.com/boost-experimental/di" class="text-info" target="_blank">boost::di</a>, <a href="https://www.arduino.cc" class="text-info" target="_blank">Arduino</a>
+                                </li>
+                                <li>
+                                    C# - <a href="https://www.microsoft.com/net/" class="text-info" target="_blank">.NET</a>, <a href="https://msdn.microsoft.com/en-us/library/bb308959.aspx" class="text-info" target="_blank">LINQ</a>, <a href="https://docs.microsoft.com/en-us/ef/" class="text-info" target="_blank">Entity Framework</a>, <a href="http://nunit.org" class="text-info" target="_blank">NUnit</a>, <a href="https://github.com/moq/moq4" class="text-info" target="_blank">Moq</a>
+                                </li>
+                                <li>
+                                    JavaScript - <a href="https://jquery.com" class="text-info" target="_blank">jQuery</a>, <a href="https://www.typescriptlang.org" class="text-info" target="_blank">TypeScript</a>
+                                </li>
+                                <li>
+                                    DevOps - <a href="https://jenkins.io" class="text-info" target="_blank">Jenkins</a>, <a href="https://travis-ci.org" class="text-info" target="_blank">TravisCI</a>, <a href="https://www.chef.io/chef/" class="text-info" target="_blank">Chef</a>, <a href="https://www.vagrantup.com" class="text-info" target="_blank">Vagrant</a>, <a href="https://www.docker.com" class="text-info" target="_blank">Docker</a>, <a href="https://www.packer.io" class="text-info" target="_blank">Packer</a>
+                                </li>
+                            </ul>
+                        </p>
+                        <p>
+                            Lastly, I'm a big fan of good processes and <a href="https://cleancoders.com/" class="text-info" target="_blank">Clean Code</a>.
+                        </p>
+                        <p class="text-center">
+                            <a href="https://github.com/dbudwin" target="_blank" class="btn btn-secondary" role="button">
+                                <span class="ion-social-github" aria-hidden="true"></span>
                                 My GitHub Page
-                        </a>
+                            </a>
+                        </p>
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-5">
                     <a href="https://github.com/dbudwin" target="_blank">
-                        <img src="images/Octocat.png" class="img-responsive" alt="GitHub Octocat">
+                        <img src="images/Octocat.png" class="img-fluid" alt="GitHub Octocat">
                     </a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                    <h2>
+            <div class="row mt-4">
+                <div class="col-xs-12 col-md-7 order-md-last">
+                    <h2 class="text-white">
                         Woodworking
                     </h2>
-                    <p>
-                        I'm really passionate about woodworking.  I'm self-taught, mainly learning from trial-and-error and <a href="https://www.youtube.com/user/Matthiaswandel" target="_blank">YouTube</a>.  Luckily, I still have all ten fingers.  I run a little woodworking business on the side mainly as a way to help fund my craft.  I really enjoy the process of starting a design in <a href="http://www.sketchup.com/" target="_blank">SketchUp</a> and seeing it through to completion.  A popular woodworking quote is, "The difference between a bad and a good woodworker is their ability to fix their mistakes."  Boy ain't that the truth because I make a lot of mistakes!
-                    </p>
-                    <div class="text-center">
-                        <a href="http://www.bluecollarsoul.com/" target="_blank" class="btn btn-primary" role="button">
-                            <span class="ionicons ion-hammer" aria-hidden="true"></span>
+                    <div class="p-3 mb-2 bg-dark text-white">
+                        <p>
+                            I'm really passionate about woodworking.  I'm self-taught, mainly learning from trial-and-error and <a href="https://www.youtube.com/user/Matthiaswandel" class="text-info" target="_blank">YouTube</a>.  Luckily, I still have all ten fingers.  I run a little woodworking business on the side mainly as a way to help fund my craft.  I really enjoy the process of starting a design in <a href="https://www.sketchup.com/" class="text-info" target="_blank">SketchUp</a> and seeing it through to completion.  A popular woodworking quote is, "The difference between a bad and a good woodworker is their ability to fix their mistakes."  Isn't that the truth because I make a lot of mistakes and I do my best to fix them!
+                        </p>
+                        <p class="text-center">
+                            <a href="https://bluecollarsoul.com" target="_blank" class="btn btn-secondary" role="button">
+                                <span class="ionicons ion-hammer" aria-hidden="true"></span>
                                 Blue Collar Soul
-                        </a>
+                            </a>
+                        </p>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                    <a href="http://www.bluecollarsoul.com/" target="_blank">
-                        <img src="images/BlueCollarSoulLogo.jpg" class="img-responsive img-rounded" alt="Blue Collar Soul">
+                <div class="col-xs-12 col-md-5">
+                    <a href="https://bluecollarsoul.com" target="_blank">
+                        <img src="images/BlueCollarSoulLogo.jpg" class="img-fluid rounded" alt="Blue Collar Soul">
                     </a>
                 </div>
             </div>
-            <div class="row">
-                <nav class="navbar navbar-default navbar-bottom" role="navigation">
-                    <div class="text-center white-text">
-                        <p>
-                            &copy; 2016-<?php echo gmdate('Y'); ?> Budw.in - Hosted on <a href="https://m.do.co/c/571a7acca6d1" target="_blank">DigitalOcean</a>
-                        </p>
-                        <p>
-                            <a href="https://twitter.com/dbudwin" target="_blank"><span class="ionicons ion-social-twitter" aria-hidden="true"></span></a>
-                            <a href="https://www.linkedin.com/in/dbudwin" target="_blank"><span class="ion-social-linkedin" aria-hidden="true"></span></a>
-                        </p>
-                    </div>
-                </nav>
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark mt-4">
+                <div class="mx-auto text-white">
+                    <span class="align-middle">
+                        &copy; 2016-<?php echo gmdate('Y'); ?> <a href="https://www.budw.in" class="text-info">Budw.in</a> - Hosted on <a href="https://m.do.co/c/571a7acca6d1" class="text-info" target="_blank">DigitalOcean</a>
+                    </span>
+                </div>
+            </nav>
         </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
 </html>
