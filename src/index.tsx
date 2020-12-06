6a2c265ba9af160e43111f6dbc382ca559599386
Drew Budwin <drew@budw.in>
2020-12-05 22:58:12 -0500

    Install Bulma and wire up SASS
    `react-bulma-components` configured the "advance" way to support tree-shaking.  Wired up SASS so that the Bulma styling could be tweaked as necessary.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import "./sass/index.scss";
+
 import App from "./app";
 import React from "react";
 import ReactDOM from "react-dom";
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     module: {
         rules: [
@@ -5,12 +7,18 @@ module.exports = {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
         ]
     },
     plugins: [
+        new MiniCssExtractPlugin(),
     ],
     resolve: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
+        modules: ["node_modules", "src/sass"],
     },
 };
