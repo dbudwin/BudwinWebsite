4bd85d0a583c86532c4e8746d85c7572d5e2df65
Drew Budwin <drew@budw.in>
2020-12-29 20:35:47 -0500

    Fix wrong type import causing error
    The missing `@` was causing issues in VSCode.

--- src/components/button-link.tsx
+++ src/components/button-link.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconProp } from "fortawesome/fontawesome-svg-core";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { SecureLink } from "react-secure-link";
 
 interface ButtonLinkProps {
