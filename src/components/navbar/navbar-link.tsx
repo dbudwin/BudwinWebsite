c01f7aa241a62e2a25a984b87b375e74f6cfa450
Drew Budwin <drew@budw.in>
2021-01-16 14:09:19 -0500

    Fix for hamburger menu not working on mobile
    Bulma doesn't ship with _any_ JS, so clicking on the hamburger menu did nothing.  This adds support to add the `is-active` class to the appropriate elements when the hamburger menu is clicked on.  Likewise, it'll remove the `is-active` class when the "X" is clicked on or when an item in the menu is clicked on so that the menu will collapse.

--- src/components/navbar/navbar-link.tsx
+++ src/components/navbar/navbar-link.tsx
@@ -4,14 +4,16 @@ import { Link } from "react-scroll";
 
 interface NavbarLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
     to: string;
+    onClick: () => void;
     offset?: number;
 }
 
-export default function NavbarLink({ to, offset, ...props }: NavbarLinkProps): ReactElement {
+export default function NavbarLink({ to, offset, onClick, ...props }: NavbarLinkProps): ReactElement {
     const scrollDurationMs = 800;
 
     return (
         <Link
+            onClick={onClick}
             className="navbar-item"
             activeClass="active"
             to={to}
@@ -19,6 +21,7 @@ export default function NavbarLink({ to, offset, ...props }: NavbarLinkProps): R
             smooth={true}
             duration={scrollDurationMs}
             offset={offset}
+            role="menuLink"
         >
             {props.children}
         </Link>
